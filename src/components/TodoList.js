edc28f2d3e7d5ef38a56342163460b3fd3ddf8c2
endermaru <endermaru@snu.ac.kr>
2023-04-11 18:32:36 +0900

    수정

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+import styles from "../styles/TodoList.module.css";
+
+const TodoList = () => {
+    return (
+        <div className={"styles.container"}>
+            <h1>Todo List</h1>
+        </div>
+    );
+}
+
+export default TodoList;
